fa152788f2fefa603ef361bae1aeded89271cc97
FShinoda <fernanda.shinoda@hotmail.com>
2024-09-06 16:36:36 -0300

    feat: Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <>
+            <header className="bg-khipo-purple text-white pt-[12px] pb-[13px] pl-[50.43px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+                <h1 className="text-[40px] leading-[48.41px] font-bold">Products</h1>
+            </header>
+        </>
+    )
+
+    
+}
